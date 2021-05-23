3d7b0f1f6c073c36951f3ac352c3149abe59c8c1
praveen <praveensaravananp@gmail.com>
2021-05-23 22:42:34 +0530

    Assignment

---
+++ Assignment20/Wrapper.java
@@ -0,0 +1,24 @@
+package com.onebill.corejava.basics;
+import java.util.*;
+public class Wrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int primi;
+		Integer nonprimi;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter Primitive Value:(int)");
+			primi=sc.nextInt();
+			nonprimi=primi;
+			System.out.println("After Autoboxing:"+nonprimi);
+			primi=nonprimi;
+			System.out.println("After Unboxing:"+primi);
+			String primi1;
+			System.out.println("Enter non Primitive value---- to int:");
+			primi1=sc.next();
+			int a=Integer.valueOf(primi1);
+			System.out.println("After Unboxing:"+a);
+		}
+	}
+
+}
